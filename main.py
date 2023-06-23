3074de67ef5c1ab01a92776472eba56e7750661f
Shouvik Ghosh <shouvikg.cal@gmail.com>
2023-06-23 11:34:18 +0530

    Changes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 with open('page.txt') as f:
     s = f.read()
 lines = [line.strip() for line in s.split('\n') if line.strip() != ""]
@@ -5,5 +7,6 @@ res = '<ul>'
 for line in lines:
     res += '<li>' + line + '</li>'
 res += '</ul>'
-with open('page.html', 'w') as f:
+os.mkdir('pages')
+with open('pages/page.html', 'w') as f:
     f.write(res)
