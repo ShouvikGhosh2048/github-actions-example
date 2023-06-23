db1e1eb3525e1553d791f0eb1e349e4777ea30f1
Shouvik Ghosh <shouvikg.cal@gmail.com>
2023-06-23 12:02:38 +0530

    Changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 with open('page.txt') as f:
     s = f.read()
@@ -7,6 +8,18 @@ res = '<ul>'
 for line in lines:
     res += '<li>' + line + '</li>'
 res += '</ul>'
-os.mkdir('pages')
+
+try:
+    response = requests.get('https://cataas.com/cat?json=true').json()
+    url = 'https://cataas.com' + response['url']
+    res += f'<img src="{url}"/>'
+except:
+    pass
+
+try:
+    os.mkdir('pages')
+except:
+    pass
+
 with open('pages/index.html', 'w') as f:
     f.write(res)
